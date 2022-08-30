f78ee337cf4fa90d7fb9a7e694c5fd55382d7ab5
esmeii <eunseong@ewhain.net>
2022-08-30 18:09:32 +0900

    esme:Add front-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>
+          <label>보유 eth: </label>
+          <span id="eth_balance"></span>
+        </div>
+        <div>
+          <label>보유 eth: </label>
+          <span id="nft_balance"></span>
+        </div>
+
+        <div>
+          <input id="pk" type="text" placeholder="PRIVATE KEY" />
+          <button id="wallet-import-btn">지갑 가져오기</button>
+        </div>
+      </div>
+      <hr></hr>
+      <div>
+        <div>
+          <label>Name </label>
+          <input type="text" id="nft-name" />
+        </div>
+        <div>
+          <label>File</label>
+          <input type="file" id="nft-file" />
+          <button id="nft-gen-btn">NFT 생성하기</button>
+        </div>
+      </div>
+      <hr></hr>
+      <div>
+        <div>
+          <label>토큰 ID</label>
+          <input
+            type="number"
+            id="nft-tokenId"
+            placeholder="토큰 ID를 입력해 주세요"
+          />
+        </div>
+        <div>
+          <label>Owner address </label>
+          <span id="nft-owner-address-search"></span>
+        </div>
+        <div>
+          <label>Name</label>
+          <span id="nft-name-search"></span>
+        </div>
+        <div>
+          <label>File path</label>
+          <span id="ipfs-path-search"></span>
+        </div>
+      </div>
+      <button id="nft-search-btn">NFT 조회하기</button>
     </div>
   );
 }
